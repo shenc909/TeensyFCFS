3a1cc72376f500c1065986c7df3acf1aa708c7ae
Shen Chen <shenc909@users.noreply.github.com>
2019-12-02 22:28:07 -0800

    added channel 5&6

--- src/TeensyFCFS/src/main.cpp
+++ src/TeensyFCFS/src/main.cpp
@@ -1,8 +1,23 @@
 #include <Arduino.h>
 #include "pindef.h"
 
-int throttleTop, throttleBtm, rollTop, rollBtm, pitchTop, pitchBtm, yawTop, yawBtm;
-volatile int throttle, roll, pitch, yaw;
+unsigned int throttleTop, throttleBtm, rollTop, rollBtm, pitchTop, pitchBtm, yawTop, yawBtm, switch1Top, switch1Btm, switch2Top, switch2Btm;
+volatile unsigned int throttle, prev_throttle, roll, prev_roll, pitch, prev_pitch, yaw, prev_yaw, switch1, prev_switch1, switch2, prev_switch2;
+unsigned int throttleOut, rollOut, pitchOut, yawOut, switch1Out, switch2Out;
+
+void rising1();
+void rising2();
+void rising3();
+void rising4();
+void rising5();
+void rising6();
+void falling1();
+void falling2();
+void falling3();
+void falling4();
+void falling5();
+void falling6();
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,11 +26,21 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(CH2), rising2, RISING);
   attachInterrupt(digitalPinToInterrupt(CH3), rising3, RISING);
   attachInterrupt(digitalPinToInterrupt(CH4), rising4, RISING);
+  attachInterrupt(digitalPinToInterrupt(CH5), rising5, RISING);
+  attachInterrupt(digitalPinToInterrupt(CH6), rising6, RISING);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  throttleOut = map(throttle, throttleBtm, throttleTop, 0, 100);
+  rollOut = map(roll, rollBtm, rollTop, -100, 100);
+  pitchOut = map(pitch, pitchBtm, pitchTop, -100, 100);
+  yawOut = map(yaw, yawBtm, yawTop, -100, 100);
+  switch1Out = map(switch1, switch1Btm, switch1Top, 0, 1);
+  rollOut = map(switch2, switch2Btm, switch2Top, 0, 1);
+  
 }
 
 /*///////////////////////////////////////////////////////////////
@@ -64,41 +89,61 @@ void calibrate() {
                         REMOTE ISRs
 *////////////////////////////////////////////////////////////////
 void rising1() {
-  attachInterrupt(0, falling1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(CH1), falling1, FALLING);
   prev_throttle = micros();
 }
  
 void falling1() {
-  attachInterrupt(0, rising1, RISING);
+  attachInterrupt(digitalPinToInterrupt(CH1), rising1, RISING);
   throttle = micros()-prev_throttle;
 }
 
 void rising2() {
-  attachInterrupt(1, falling2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(CH2), falling2, FALLING);
   prev_roll = micros();
 }
  
 void falling2() {
-  attachInterrupt(1, rising2, RISING);
+  attachInterrupt(digitalPinToInterrupt(CH2), rising2, RISING);
   roll = micros()-prev_roll;
 }
 
 void rising3() {
-  attachInterrupt(2, falling3, FALLING);
+  attachInterrupt(digitalPinToInterrupt(CH3), falling3, FALLING);
   prev_pitch = micros();
 }
  
 void falling3() {
-  attachInterrupt(2, rising3, RISING);
+  attachInterrupt(digitalPinToInterrupt(CH3), rising3, RISING);
   pitch = micros()-prev_pitch;
 }
 
 void rising4() {
-  attachInterrupt(3, falling4, FALLING);
+  attachInterrupt(digitalPinToInterrupt(CH4), falling4, FALLING);
   prev_yaw = micros();
 }
  
 void falling4() {
-  attachInterrupt(3, rising4, RISING);
+  attachInterrupt(digitalPinToInterrupt(CH4), rising4, RISING);
   yaw = micros()-prev_yaw;
+}
+
+void rising5() {
+  attachInterrupt(digitalPinToInterrupt(CH5), falling5, FALLING);
+  prev_switch1 = micros();
+}
+ 
+void falling5() {
+  attachInterrupt(digitalPinToInterrupt(CH5), rising5, RISING);
+  switch1 = micros()-prev_switch1;
+}
+
+void rising6() {
+  attachInterrupt(digitalPinToInterrupt(CH6), falling6, FALLING);
+  prev_switch2 = micros();
+}
+ 
+void falling6() {
+  attachInterrupt(digitalPinToInterrupt(CH6), rising6, RISING);
+  switch2 = micros()-prev_switch2;
 }
